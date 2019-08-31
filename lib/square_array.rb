1e666325b476e192e200bb41783f0560942ca265
Kim Lewis <kalewi2@gmail.com>
2019-08-31 20:33:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,12 +8,3 @@ end
   return new_numbers
 end
 
-def square_array2(new_numbers)
-  two_new_numbers = []
-  i = 0
-  while i < square_array.length
-    two_new_numbers.push(n ** 2)
-    i += 1
-  end
- return two_new_numbers
-end
