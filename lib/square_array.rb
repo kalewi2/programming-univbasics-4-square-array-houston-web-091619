85fd5abdeee50091deb0b496db2bc2e6128923e1
Kim Lewis <kalewi2@gmail.com>
2019-08-31 20:26:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,16 @@
 def square_array(numbers)
   new_numbers = []
-  numbers.collect do {|n| n ** 2}
+   
   
 end
   return new_numbers
+end
+square_array(new_numbers)
+  two_new_numbers = []
+  i = 0
+  while new_numbers[i]
+    two_new_numbers.push(n ** 2)
+    +=i
+  end
+ return two_new_numbers
 end
